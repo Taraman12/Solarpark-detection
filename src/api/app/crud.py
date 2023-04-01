758761f866c16abdcf75b93a2b8dd4585ccede49
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-04-01 13:29:51 +0200

    initial commit

---
+++ src/api/app/crud.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_solar_plant(db: Session, id_plant: int):
+    return (
+        db.query(models.SolarPlants).filter(models.SolarPlants.id == id_plant).first()
+    )
+
+
+def create_solar_plant(db: Session, solar_plant: schemas.SolarPlantsCreate):
+    db_solar_plant = models.SolarPlants(**solar_plant.dict())
+    db.add(db_solar_plant)
+    db.commit()
+    db.refresh(db_solar_plant)
+    return db_solar_plant
---
+++ src/api/app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Date, Float, Integer, String
+
+from .database import Base
+
+# from sqlalchemy.orm import relationship
+
+
+class SolarPlants(Base):
+    __tablename__ = "solar_plants"
+
+    id_plant = Column(Integer, primary_key=True, index=True)
+    size_in_sq_m = Column(Float)
+    peak_power = Column(Float)
+    date_of_data = Column(Date)
+    first_detection = Column(Date)
+    last_detection = Column(Date)
+    geometry = Column(String, unique=True)
