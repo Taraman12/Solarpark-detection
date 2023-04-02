b19d94c738b55c6a979f93c63baf4f82a75c110b
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-04-02 20:52:15 +0200

    added docker support for frontend

--- src/frontend/src/views/TestView.vue
+++ src/frontend/src/views/TestView.vue
@@ -1,7 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+const data = ref(null)
+const error = ref(null)
+
+const options = {
+  method: "GET",
+  mode: "cors",
+  headers: {
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+}
+
+fetch("http://localhost:5000/", options)
+.then((res) => res.json())
+.then((json) => (data.value = json))
+.then(data => console.log(data))
+.catch((err) => (error.value = err))
+</script>
+
 <template>
     <div class="about">
       <h1>This is my first test page</h1>
-    </div>
+      <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+      <div v-else-if="data">
+          Data loaded:
+          <pre>{{ data.message }}</pre>
+      </div>
+      <div v-else>Loading...</div>
+  </div>
   </template>
   
   <style>
