9d737ccec239fc15d03a1cc2e09e41785ec1c5e5
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-04-29 13:16:42 +0200

    initial commit

---
+++ src/preprocess_data_to_disk/constants.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import re
+
+IMAGE_INPUT_DIR = Path(r".\data_local\training_data_raw")
+
+MASK_INPUT_DIR = Path(r".\src\preprocess_data_to_disk\data\trn_polygons_germany_tile_names.geojson")
+
+IMAGE_OUTPUT_DIR = Path(r".\data_local\images_only_AOI_test_season")
+
+MASK_OUTPUT_DIR = Path(r".\data_local\mask_only_AOI_test_season")
+
+IDENTIFIER_REGEX = re.compile(
+    r"""^(?P<mission>S2[A-B])_MSI
+                    (?P<product_level>L[1-2][A-C])_
+                    (?P<sensing_time>\d{8}T\d{6})_
+                    (?P<processing_baseline>N\d{4})_
+                    (?P<relative_orbit>R\d{3})_T
+                    (?P<utm_code>\d{2})
+                    (?P<latitude_band>\w{1})
+                    (?P<square>\w{2})_
+                    (?P<year>\d{4})
+                    (?P<month>\d{2})
+                    (?P<day>\d{2})T
+                    (?P<product_time>\d{6})""",
+    re.VERBOSE,
+)
---
+++ src/preprocess_data_to_disk/main_preprocess.py
@@ -0,0 +1,75 @@
+# build-in
+import os
+import re
+from pathlib import Path
+from typing import Any, Dict, Tuple, Union
+
+# third-party
+import geopandas as gpd
+import numpy as np
+import rasterio
+import torch
+from rasterio import DatasetReader
+from torch import Tensor
+from torchvision.transforms import Pad
+
+# local modules
+import constants as c
+from preprocessing_dataset_to_disk import save_patched_data_to_disk 
+
+if __name__ == "__main__":
+    print("Program started")
+    root_dir = Path(__file__).resolve().parent.parent
+    # os.chdir(Path(__file__).parent)
+    print(os.getcwd())
+    # rename
+    image_input_dir = c.IMAGE_INPUT_DIR
+    mask_input_dir = c.MASK_INPUT_DIR  # root_dir
+    image_output_dir = c.IMAGE_OUTPUT_DIR
+    mask_output_dir = c.MASK_OUTPUT_DIR
+
+    for input_directory in [image_input_dir, mask_input_dir]:
+        if not input_directory.exists():
+            print(f"Input path: {input_directory} does not exist")
+
+    for output_directory in [image_output_dir, mask_output_dir]:
+        if not output_directory.exists():
+            output_directory.mkdir(parents=True, exist_ok=False)
+            print(
+                f"Output path: {output_directory} does not exist \n"
+                f"Directory created"
+            )
+    
+    masks_gdf = gpd.read_file(mask_input_dir)
+
+    kernel_size = 256
+    # ToDo: add loop over all folders in image_dir (it is only a single one)
+    # open mask_gdf outside the loop
+    for tile_folder in os.listdir(image_input_dir):
+        # ! changed to match instead of search
+        regex_match = re.match(c.IDENTIFIER_REGEX, tile_folder)
+
+        if not regex_match:
+            continue
+
+        else:
+            utm_code = regex_match.group("utm_code")
+            latitude_band = regex_match.group("latitude_band")
+            square = regex_match.group("square")
+            year = regex_match.group("year")
+            month = str(int(regex_match.group("month")))
+            day = str(int(regex_match.group("day")))
+            tile = f"{utm_code}{latitude_band}{square}"
+
+       
+        result = save_patched_data_to_disk(
+            image_input_dir / tile_folder,
+            masks_gdf,
+            image_output_dir,
+            mask_output_dir,
+            kernel_size,
+            tile=tile,
+            tile_date=f"{year}-{month}-{day}",
+        )
+        print(f"Number of files saved: {result}")
+    print("program finished")
