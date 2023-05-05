fe7e3ca8734666d87297ae230039e4bb84501be6
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-05-05 09:12:21 +0200

    formatting / linting

--- src/ml-serve/app/test.py
+++ src/ml-serve/app/test.py
@@ -1,9 +1,7 @@
 # test
 import yaml
-import os
 
-with open (r"src/ml-serve/app/model/model-config.yaml", "r") as f:
+with open(r"src/ml-serve/app/model/model-config.yaml", "r") as f:
     model_config = yaml.safe_load(f)
 
 print(model_config["encoder_name"])
-
