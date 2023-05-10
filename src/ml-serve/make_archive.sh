00f3d00ebbe033bb2fb5ef13b5139a32fb624e63
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-05-10 14:34:59 +0200

    changed paths for execution

--- src/ml-serve/make_archive.sh
+++ src/ml-serve/make_archive.sh
@@ -3,11 +3,11 @@
 
 torch-model-archiver --model-name test_model \
                      --version 1.0 \
-                     --serialized-file ml-serve/model-store/eff_b0.pth \
-                     --handler ml-serve/model-store/handler_new.py \
-                     --extra-files ml-serve//model-store/model-config.yaml \
-                     --requirements-file ml-serve/requirements.txt \
-                     --export-path ml-serve/model-store \
+                     --serialized-file model-store/eff_b0.pth \
+                     --handler model-store/handler.py \
+                     --extra-files model-store/model-config.yaml \
+                     --requirements-file requirements.txt \
+                     --export-path model-store \
                      --force
-                    # BUG mentioned in the docs but doesn't work 
+                    # Will be available at torchserve version 0.8.0 (current 0.7.1)
                     # --config-file ml-serve/app/model-store/model-config.yaml
