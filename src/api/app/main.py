6d0558a178d498527c4bd1ffdcf8c02a0c701ccf
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-05-22 20:09:57 +0200

    refactored api

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -1,66 +1,25 @@
-# import uvicorn
-from fastapi import Depends, FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
+# third party
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+# local modules
+from app.api_core.api_v1.api import api_router
+from app.core.config import settings
 
-models.Base.metadata.create_all(bind=engine)
+# os.chdir(Path(__file__).parent)
 
-app = FastAPI()
-
-origins = [
-    "https://localhost.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:5173",
-    "http://localhost:*",
-    "172.19.0.1:34048",
-    "172.19.0.1:*",
-    "172.20.0.1" "*",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World!"}
-
-
-@app.post("/solar_plants/", response_model=schemas.SolarPlants)
-def create_solar_plant(
-    solar_plant: schemas.SolarPlantsCreate, db: Session = Depends(get_db)
-):
-    # db_solar_plant = crud.get_solar_plant(db, id_plant=id_plant)
-    # if db_solar_plant:
-    #    raise HTTPException(status_code=400, detail="Solar plant already registered")
-    return crud.create_solar_plant(db=db, solar_plant=solar_plant)
-
-
-@app.get("/solar_plants/{id_plant}", response_model=schemas.SolarPlants)
-def read_solar_plant(id_plant: int, db: Session = Depends(get_db)):
-    db_solar_plant = crud.get_solar_plant(db, id_plant=id_plant)
-    if db_solar_plant is None:
-        raise HTTPException(status_code=404, detail="Solar plant not found")
-    return db_solar_plant
-
-
-# if __name__ == "__main__":
-#    uvicorn.run(app, host="127.0.0.1", port=8000)
+app.include_router(api_router, prefix=settings.API_V1_STR)
