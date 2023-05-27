dc45e8f632f4d634d90dcc35da0db363b2d6c797
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-05-27 19:38:49 +0200

    initial commit

---
+++ src/frontend/vue.config.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'index.html',
+            filename: 'index.html'
+        }
+    }
+}
