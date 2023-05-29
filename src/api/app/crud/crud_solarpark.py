478905013d5dae1c82853d377ce649b2ec3e0a29
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-05-29 17:14:27 +0200

    added export to geojson

--- src/api/app/crud/crud_solarpark.py
+++ src/api/app/crud/crud_solarpark.py
@@ -3,16 +3,20 @@ from typing import Any, TypeVar
 
 # third-party
 # from pydantic import BaseModel
+from fastapi.responses import StreamingResponse, Response
 from fastapi.encoders import jsonable_encoder
-from shapely.geometry import Polygon
-from sqlalchemy.orm import Session
 
-from app.db.base_class import Base
-from app.models.solarpark import SolarPark
-from app.schemas.solarpark import SolarParkCreate, SolarParkUpdate
+# from shapely.geometry import Polygon
+from sqlalchemy.orm import Session
+from geojson import Feature, FeatureCollection, Polygon
+import geojson
+
 
 # local modules
 from .base import CRUDBase
+from app.db.base_class import Base
+from app.models.solarpark import SolarPark
+from app.schemas.solarpark import SolarParkCreate, SolarParkUpdate
 
 # import shapely.wkt
 from geoalchemy2.elements import WKTElement
@@ -23,8 +27,59 @@ from geoalchemy2.elements import WKTElement
 ModelType = TypeVar("ModelType", bound=Base)
 
 
+# GeoJSON-Datei als Stream senden
+def generate(feature_collection: FeatureCollection):
+    yield '{"type": "FeatureCollection", "features": ['
+    for i, feature in enumerate(feature_collection["features"]):
+        if i > 0:
+            yield ","
+        yield str(feature)
+    yield "]}"
+
+
 class CRUDSolarPark(CRUDBase[SolarPark, SolarParkCreate, SolarParkUpdate]):
-    pass
+    # pass
+    def get_geojson(
+        self,
+        db: Session,
+    ) -> Any:
+        db_obj = db.query(SolarPark).all()
+        obj_data = jsonable_encoder(db_obj)
+        # GeoJSON-FeatureCollection erstellen
+        features = []
+        for row in obj_data:
+            lat = row["lat"]
+            lon = row["lon"]
+            coords = [(lon[i], lat[i]) for i in range(len(lat))]
+            polygon = Polygon([coords])
+            properties = {
+                "name_of_model": row["name_of_model"],
+                "size_in_sq_m": row["size_in_sq_m"],
+                "peak_power": row["peak_power"],
+                "date_of_data": row["date_of_data"],  # .strftime('%Y-%m-%d'),
+                "first_detection": row["first_detection"],  # .strftime('%Y-%m-%d'),
+                "last_detection": row["last_detection"],  # .strftime('%Y-%m-%d'),
+                "avg_confidence": row["avg_confidence"],
+            }
+            feature = Feature(geometry=polygon, properties=properties)
+            features.append(feature)
+        feature_collection = FeatureCollection(features)
+           # GeoJSON-Datei als Stream senden
+        async def generate():
+            yield '{"type": "FeatureCollection", "features": ['
+            for i, feature in enumerate(feature_collection['features']):
+                if i > 0:
+                    yield ','
+                yield str(feature)
+            yield ']}'
+        # geojson_data = str(feature_collection).encode("utf-8")
+        # response.headers["Content-Disposition"] = "attachment; filename=geodata.geojson"
+        return StreamingResponse(
+            content=generate(),
+            media_type="application/json",
+            headers={"Content-Disposition": "attachment; filename=geodata.geojson"},
+        )
+
     # def get(self, db: Session, id: Any, *, polygon: bool = False) -> SolarPark:
     #     if polygon:
     #         db_obj = db.query(SolarPark).filter(SolarPark.id == id).first()
@@ -40,8 +95,6 @@ class CRUDSolarPark(CRUDBase[SolarPark, SolarParkCreate, SolarParkUpdate]):
     #         db_obj.geometry = Polygon(db_obj.geometry)
     #         return db_obj
     #     return db.query(SolarPark).offset(skip).limit(limit).all()
-
-
 
     # pass
     # def create(self, db: Session, *, obj_in: SolarParkCreate):
