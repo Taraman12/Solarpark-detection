51b6c38fe530a6e190e6fae13385a4fbbe4107a0
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-04 20:16:42 +0200

    initial commit

---
+++ src/frontend/src/layouts/LayoutBase.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+import { useStyleStore } from "@/stores/style.js";
+
+const styleStore = useStyleStore();
+
+const layoutAsidePadding = "xl:pl-60";
+
+const isAsideMobileExpanded = ref(false);
+
+</script>
+
+<template>
+    <div :class="{
+        dark: styleStore.darkMode,
+        'overflow-hidden lg:overflow-visible': isAsideMobileExpanded,
+    }">
+        <div :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': isAsideMobileExpanded }]"
+            class="pt-14 min-h-screen w-screen transition-position lg:w-auto bg-gray-50 dark:bg-neutral-800 dark:text-neutral-200">
+            <slot></slot>
+        </div>
+    </div>
+</template>
