5448c636771bd1ccbd02dd70a23d6add4b87b256
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-04 20:17:35 +0200

    added ml-serve route

--- src/api/app/api_core/api_v1/endpoints/root.py
+++ src/api/app/api_core/api_v1/endpoints/root.py
@@ -1,5 +1,6 @@
 # third-party
 from fastapi import APIRouter
+import requests
 
 router = APIRouter()
 
@@ -7,3 +8,10 @@ router = APIRouter()
 @router.get("/")
 def root():
     return {"message": "Hello World!"}
+
+
+@router.get("/ml-server")
+def ml_server():
+    response = requests.get("http://localhost:8081/models")
+    # return json response
+    return response.json()["models"][0]
