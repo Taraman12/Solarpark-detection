69b537247c8bfb11bf60e83ab73274e777156158
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-04 20:19:45 +0200

    renamed user/pw

--- src/downloader/app/sentinel_api.py
+++ src/downloader/app/sentinel_api.py
@@ -9,9 +9,9 @@ from sentinelsat import SentinelAPI
 
 def connect_to_sentinel_api() -> Union[SentinelAPI, Exception]:
     load_dotenv()
-    api_user = os.getenv("API_USER")
-    api_secret = os.getenv("API_SECRET")
-    api_url = os.getenv("API_URL")
+    api_user = os.getenv("COPERNICUS_API_USER")
+    api_secret = os.getenv("COPERNICUS_API_SECRET")
+    api_url = os.getenv("COPERNICUS_API_URL")
     api = SentinelAPI(api_user, api_secret, api_url)
     # Connect to the Sentinel API
     try:
