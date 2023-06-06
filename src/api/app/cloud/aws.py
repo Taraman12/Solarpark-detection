4380f2a989955d9a4cd926890ff29c540ec00292
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-06 22:16:24 +0200

    added logger and aws credentials

--- src/api/app/cloud/aws.py
+++ src/api/app/cloud/aws.py
@@ -1,12 +1,15 @@
 # build-in
-import logging
 import os
 
 # third-party
 import boto3
 import docker
 from botocore.errorfactory import ClientError
+
+from botocore.credentials import InstanceMetadataFetcher, InstanceMetadataProvider
+
 from dotenv import load_dotenv
+from logging_config import get_logger
 
 # local-modules
 
@@ -32,9 +35,40 @@ session = boto3.Session(
     region_name=region_name,
 )
 
+logger = get_logger("BaseConfig")
+# local file
+if os.environ.get("LOCAL"):
+    load_dotenv()
+
+# In local docker environment, the AWS credentials are stored in the .env file
+if os.environ.get("AWS_ACCESS_KEY_ID") and os.environ.get("AWS_SECRET_ACCESS_KEY"):
+    session = boto3.Session(
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.environ.get("AWS_REGION"),
+    )
+
+# if deployed on aws ec2 instance, the credentials are provided by the IAM role of the ec2 instance
+else:
+    logger.info("Using IAM role credentials")
+    provider = InstanceMetadataProvider(
+        iam_role_fetcher=InstanceMetadataFetcher(timeout=1000, num_attempts=2)
+    )
+    if provider is None:
+        raise ValueError("No IAM role found")
+    credentials = provider.load().get_frozen_credentials()
+    logger.info(f"Credentials:{credentials.access_key}")
+    session = boto3.Session(
+        aws_access_key_id=credentials.access_key,
+        aws_secret_access_key=credentials.secret_key,
+        aws_session_token=credentials.token,
+        region_name="eu-central-1",
+    )
+
+
 # create s3 client
 s3_client = session.client("s3")
-ec2_client = session.client("ec2", region_name=os.getenv("region_name"))
+ec2_client = session.client("ec2", region_name="eu-central-1")
 
 
 def verify_aws_credentials() -> bool:
@@ -50,7 +84,6 @@ aws_available = verify_aws_credentials()
 
 
 client = docker.from_env()
-logging.info(client.containers.list())
 
 
 EC2_KWARGS = {
