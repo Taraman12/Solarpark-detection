832c0c7b5da884fb3494b770312a7b002953cfd0
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-25 12:38:40 +0200

    initial commit

---
+++ src/frontend/src/components/FKButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+    color: {
+        type: String,
+        default: 'grey'
+    },
+})
+</script>
+
+<template>
+    <button :class="[
+        'bg-' + color + '-500',
+        'hover:bg-' + color + '-700',
+        'text-white',
+        'font-bold',
+        'py-2',
+        'px-4',
+        'rounded',
+        'inline-flex',
+        'items-center',
+        'focus:outline-none',
+        'focus:shadow-outline',
+    ]" @click="$emit('click')">
+        <slot />
+    </button>
+</template>
