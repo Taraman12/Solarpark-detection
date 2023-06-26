b7bb4864d72145f3006e5fee7d8bbccdf5cd45a6
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-26 23:35:54 +0200

    tests

--- src/api/app/api_core/api_v1/endpoints/root.py
+++ src/api/app/api_core/api_v1/endpoints/root.py
@@ -1,6 +1,10 @@
 # third-party
 import requests
 from fastapi import APIRouter
+# import os
+# import subprocess
+
+# from app.preprocessing.main_preprocessing import main_preprocessing
 
 router = APIRouter()
 
@@ -15,3 +19,12 @@ def ml_server():
     response = requests.get("http://ml-serve:8081/models")
     # return json response
     return response.json()["models"][0]
+
+
+# @router.get("/start-preprocess")
+# async def start_preprocess():
+#     # await os.system("type nul > filename.txt")
+#     print("start preprocess")
+#     await subprocess.run(["type nul", ">", "filename.txt"])
+#     # await main_preprocessing()
+#     return {"message": "start preprocess"}
