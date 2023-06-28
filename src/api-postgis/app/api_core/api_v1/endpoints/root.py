37f06e6ca06766c14ab3dc45037350844948f8e9
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-28 02:46:13 +0200

    moved to archive

--- src/api-postgis/app/api_core/api_v1/endpoints/root.py
+++
@@ -1,9 +0,0 @@
-# third-party
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/")
-def root():
-    return {"message": "Hello World!"}
--- src/api-postgis/app/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .maillist import MailList  # noqa
-from .solarpark import SolarPark  # noqa
