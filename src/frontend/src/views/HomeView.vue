634f6515ad37594548b62ed261352a5ad1ddc4ef
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-06-28 02:18:32 +0200

    changed layout

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main class="mx-auto max-w-screen-xl">
+  <main class="mx-auto max-w-2xl">
     <h1 class="text-2xl">
       Continues Solar Park Detection
     </h1>
