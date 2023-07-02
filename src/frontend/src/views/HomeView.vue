03665c5f31e36c9e144c17bc76c514919863214c
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-07-02 21:11:12 +0200

    minor change

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -3,14 +3,17 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main class="mx-auto max-w-2xl">
+  <main class="mx-auto max-w-2xl mt-4">
     <h1 class="text-2xl">
       Continues Solar Park Detection
     </h1>
     <h2>Welcome</h2>
+    <h3>
     On this website you will find information about my master’s thesis titled “Continues Solar Park
-    Detection”. In this project, I used a neural network to continuously detect solar parks from Sentinel-2 data. On the
+    Detection”.
+    </h3>
+    In this project, I used a neural network to continuously detect solar parks from Sentinel-2 data. On the
     website, you can view the results and interactively explore them on a map. You also have the option to validate the
-    data.”
+    data.
   </main>
 </template>
