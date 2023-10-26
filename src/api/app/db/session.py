0a8394a0b2e194aa0447da42bae1efcc4cabf9f2
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-10-26 14:35:02 +0200

    changed to pydantic v2

--- src/api/app/db/session.py
+++ src/api/app/db/session.py
@@ -1,10 +1,29 @@
 # third-party
+from functools import lru_cache
+from typing import Optional
+
+from pydantic import PostgresDsn
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # local modules
 from app.core.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+
+# see: https://fastapi.tiangolo.com/advanced/settings/?h=envir
+@lru_cache()
+def get_settings():
+    return settings
+
+
+SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = PostgresDsn.build(
+    scheme="postgresql",
+    username=settings.USER,
+    password=settings.PASSWORD,
+    host=settings.HOST,
+    path=settings.DB,
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI.unicode_string(), pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
