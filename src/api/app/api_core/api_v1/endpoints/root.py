3c4770db3cf3ade29121b9d5528f3097c790b173
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-10-31 20:47:12 +0100

    code cleaning

--- src/api/app/api_core/api_v1/endpoints/root.py
+++ src/api/app/api_core/api_v1/endpoints/root.py
@@ -1,23 +1,9 @@
 # third-party
 from fastapi import APIRouter
 
-# import os
-# import subprocess
-
-# from app.preprocessing.main_preprocessing import main_preprocessing
-
 router = APIRouter()
 
 
 @router.get("/")
 def root():
     return {"message": "API is running"}
-
-
-# @router.get("/start-preprocess")
-# async def start_preprocess():
-#     # await os.system("type nul > filename.txt")
-#     print("start preprocess")
-#     await subprocess.run(["type nul", ">", "filename.txt"])
-#     # await main_preprocessing()
-#     return {"message": "start preprocess"}
