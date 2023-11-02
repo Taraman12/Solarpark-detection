0dd641a918b3b8fc7fb582a6f13628657b811d99
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-11-02 20:40:12 +0100

    refactored checks

---
+++ src/preprocessing/app/health_checks.py
@@ -0,0 +1,92 @@
+from typing import List
+from pathlib import Path
+import requests
+import time
+
+
+from logging_config import get_logger
+from constants import (
+    IMAGE_INPUT_DIR,
+    IMAGE_OUTPUT_DIR,
+    MASK_INPUT_DIR,
+    MASK_OUTPUT_DIR,
+    URL_API,
+    URL_ML,
+)
+
+logger = get_logger("BaseConfig")
+
+
+def check_input_paths(input_dirs: List[Path]) -> bool:
+    """Validates that the input directories exist.
+
+    Args:
+        input_dirs (List[Path]): A list of input directories to validate.
+        logger: A logger instance to use for logging.
+
+    Returns:
+        bool: True if all input directories exist, False otherwise.
+    """
+    all_dirs_exist = True
+    for input_dir in input_dirs:
+        if not input_dir.exists():
+            logger.error(f"Input path: {input_dir} does not exist")
+            all_dirs_exist = False
+    return all_dirs_exist
+
+
+def check_ml_serve_online() -> bool:
+    retries = 3
+    while retries > 0:
+        try:
+            logger.info(f"Checking if TorchServe is running on {URL_ML}")
+            response = requests.get(f"{URL_ML}/ping")
+            if response.status_code == 200:
+                logger.info("TorchServe is running")
+                return True
+            else:
+                logger.info("TorchServe is not running. retry in 5 seconds.")
+                time.sleep(5)
+        except requests.exceptions.ConnectionError:
+            logger.info("TorchServe is not running. Retry in 5 seconds.")
+            time.sleep(5)
+        retries -= 1
+    return False
+
+
+def check_ml_serve_online_localhost() -> bool:
+    retries = 3
+    while retries > 0:
+        try:
+            logger.info(f"Checking if TorchServe is running on {URL_ML}")
+            response = requests.get("http://localhost:8080/ping")
+            if response.status_code == 200:
+                logger.info("TorchServe is running")
+                return True
+            else:
+                logger.info("TorchServe is not running. retry in 5 seconds.")
+                time.sleep(5)
+        except requests.exceptions.ConnectionError:
+            logger.info("TorchServe is not running. Retry in 5 seconds.")
+            time.sleep(5)
+        retries -= 1
+    return False
+
+
+def check_api_online() -> bool:
+    retries = 3
+    while retries > 0:
+        try:
+            logger.info(f"Checking if API is running on {URL_API}")
+            response = requests.get(f"{URL_API}")
+            if response.status_code == 200:
+                logger.info("API is running")
+                return True
+            else:
+                logger.info("API is not running. retry in 5 seconds.")
+                time.sleep(5)
+        except requests.exceptions.ConnectionError:
+            logger.info("API is not running. Retry in 5 seconds.")
+            time.sleep(5)
+        retries -= 1
+    return False
