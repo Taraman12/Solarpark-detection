ae37e16625c70ac3a0f423a6e3d5a1c808421e2e
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-11-14 00:15:46 +0100

    added to_string method and extra no_leading_zeros for date

--- src/preprocessing/app/tests/test_models_identifier.py
+++ src/preprocessing/app/tests/test_models_identifier.py
@@ -14,11 +14,14 @@ def test_identifier_happy_path():
     assert identifier.latitude_band == "A"
     assert identifier.square == "BC"
     assert identifier.year == "2022"
-    assert identifier.month == "1"
-    assert identifier.day == "1"
+    assert identifier.month == "01"
+    assert identifier.day == "01"
+    assert identifier.month_no_leading_zeros == "1"
+    assert identifier.day_no_leading_zeros == "1"
     assert identifier.product_time == "123456"
     assert identifier.tile == "01ABC"
-    assert identifier.tile_date == "2022-1-1"
+    assert identifier.tile_date == "2022-01-01"
+    assert identifier.tile_date_no_leading_zeros == "2022-1-1"
 
 
 def test_identifier_unhappy_path():
@@ -26,3 +29,10 @@ def test_identifier_unhappy_path():
     identifier_string = "S2A_MSIL1C_20220101T123456_R123_T01ABC_20220101T123456"
     with pytest.raises(ValueError):
         Identifier(identifier_string)
+
+
+def test_identifier_to_string():
+    identifier_string = "S2A_MSIL1C_20220101T123456_N0302_R123_T01ABC_20220101T123456"
+    identifier = Identifier(identifier_string)
+    print(identifier.to_string())
+    assert identifier.to_string() == identifier_string
