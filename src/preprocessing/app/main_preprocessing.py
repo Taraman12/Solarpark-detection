76d923e1d3fe89dbee76760af3e337d0a897124f
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-11-17 16:22:53 +0100

    refactored

--- src/preprocessing/app/main_preprocessing.py
+++ src/preprocessing/app/main_preprocessing.py
@@ -1,9 +1,13 @@
 # build-in
 import copy
 from pathlib import Path
+from typing import Any, Dict, Tuple, Union
+
+import geopandas as gpd
+import numpy as np
 
 # third-party
-import debugpy
+# import debugpy
 import rasterio
 
 # local-modules
@@ -11,6 +15,7 @@ from constants import (
     IMAGE_OUTPUT_DIR,
     KERNEL_SIZE,
     MASK_OUTPUT_DIR,
+    NOW_DICT,
     PATH_TO_TILES,
     STEP_SIZE,
     URL_API,
@@ -19,14 +24,14 @@ from constants import (
 from health_checks import run_checks
 from jwt_functions import get_jwt_from_api, store_jwt
 from logging_config import get_logger
-
-# from models.identifier import Identifier
+from models.identifier import Identifier
 from open_data import open_data_handler
 from prediction import prediction_handler
 from preprocess import moving_window, pad_image, preprocess_handler
+from rasterio.windows import Window
 from sentinel_on_aws import download_from_sentinel_aws_handler
 from sentinel_query import get_identifier_handler
-from settings import DOCKERIZED, MAKE_TRAININGS_DATA, PRODUCTION
+from settings import DOCKERIZED  # , MAKE_TRAININGS_DATA, PRODUCTION
 from utils import create_output_directories, load_tiles_file, update_metadata
 from write_to_db import write_to_db_handler
 
@@ -34,31 +39,31 @@ from write_to_db import write_to_db_handler
 logger = get_logger("BaseConfig")
 
 # Vscode remote debugging in docker
-debugpy.listen(("0.0.0.0", 5678))
+# debugpy.listen(("0.0.0.0", 5678))
 """
 ToDo: Add check if image and mask have the same length
 ToDo: Log/logger.info the number of total files saved
 """
 
 
-# ! don't delete yet
-# def check_ml_serve_online_localhost() -> bool:
-#     retries = 3
-#     while retries > 0:
-#         try:
-#             logger.info(f"Checking if TorchServe is running on {URL_ML}")
-#             response = requests.get("http://localhost:8080/ping")
-#             if response.status_code == 200:
-#                 logger.info("TorchServe is running")
-#                 return True
-#             else:
-#                 logger.info("TorchServe is not running. retry in 5 seconds.")
-#                 time.sleep(5)
-#         except requests.exceptions.ConnectionError:
-#             logger.info("TorchServe is not running. Retry in 5 seconds.")
-#             time.sleep(5)
-#         retries -= 1
-#     return False
+def main():
+    logger.info(f"Dockerized = {DOCKERIZED}")
+    logger.info(f"URL_ML = {URL_ML}")
+    logger.info(f"URL_API = {URL_API}")
+
+    run_setup()
+    run_checks()
+
+    tiles_gdf = load_tiles_file(path=PATH_TO_TILES)
+    tiles_list = ["32UQE"]
+
+    for tile_counter, tile_name in enumerate(tiles_list):
+        # logger.info(f"Processing tile {tile_counter+1} of {len(set(tiles_gdf.tile_name))}")
+        try:
+            process_tile(tile_name, tiles_gdf)
+        except Exception as e:
+            logger.error(f"Could not process tile {tile_name}: {e}")
+            continue
 
 
 def run_setup() -> None:
@@ -75,139 +80,146 @@ def run_setup() -> None:
         exit(1)
 
 
-if __name__ == "__main__":  # noqa C901
-    logger.info(f"Dockerized = {DOCKERIZED}")
-    logger.info(f"Make_trainings_data = {MAKE_TRAININGS_DATA}")
-    logger.info(f"Production = {PRODUCTION}")
-    logger.info(f"URL_ML = {URL_ML}")
-    logger.info(f"URL_API = {URL_API}")
+def process_tile(tile_name: str, tiles_gdf: gpd.GeoDataFrame):
+    identifier, band_file_info = get_identifier_and_band_info(tile_name=tile_name)
+    if identifier is None or band_file_info is None:
+        return
 
-    run_setup()
-    run_checks()
+    stacked_bands, first_band_open, metadata = open_and_preprocess_data(
+        band_file_info=band_file_info
+    )
+    if stacked_bands is None:
+        return
 
-    tiles_gdf = load_tiles_file(path=PATH_TO_TILES)
+    num_rows, num_cols = calc_rows_cols(metadata)
 
-    target_folder = Path("/tmp")
+    file_identifier = 0
+    # use
+    # from concurrent.futures import ThreadPoolExecutor
+    # from itertools import product
+    # to improve speed
 
-    # TODO: Include Case
-    # if MAKE_TRAININGS_DATA:
-    #     folder_list = os.listdir(IMAGE_INPUT_DIR)
+    for row in range(num_rows + 1):
+        for col in range(num_cols + 1):
+            # Define the window coordinates for the snippet
+            window = rasterio.windows.Window(
+                col * STEP_SIZE, row * STEP_SIZE, KERNEL_SIZE, KERNEL_SIZE
+            )
+            file_identifier += 1
+            process_window(
+                stacked_bands=stacked_bands,
+                window=window,
+                metadata=metadata,
+                first_band_open=first_band_open,
+                identifier=identifier,
+                file_identifier=file_identifier,
+            )
+    return
 
-    dates = {"start_date": "2021-01-01", "end_date": "2021-01-31"}
-    tiles_list = ["32UQE"]
-    for tile_counter, tile_name in enumerate(
-        tiles_list
-    ):  # enumerate(set(tiles_gdf.tile_name)):
-        logger.info(
-            f"Processing tile {tile_counter+1} of {len(set(tiles_gdf.tile_name))}"
+
+def get_identifier_and_band_info(
+    tile_name: str,
+) -> Tuple[Identifier | None, Dict[str, Dict[str, Union[str, Path]]] | None]:
+    identifier = get_identifier_handler(tile_id=tile_name, dates=NOW_DICT)
+    if identifier is None:
+        return None, None
+
+    logger.info(f"Starting download from sentinel aws for {identifier.to_string()}")
+    band_file_info = download_from_sentinel_aws_handler(
+        identifier=identifier, target_folder=IMAGE_OUTPUT_DIR
+    )
+    return identifier, band_file_info
+
+
+def open_and_preprocess_data(
+    band_file_info: Dict[str, Dict[str, Union[str, Path]]]
+) -> Tuple[np.ndarray | None, dict, Dict[str, Any] | None]:
+    stacked_bands, first_band_open, original_metadata = open_data_handler(
+        band_file_info=band_file_info
+    )
+    if stacked_bands is None:
+        return None, None, None
+
+    metadata = copy.deepcopy(original_metadata)
+    stacked_bands = preprocess_handler(array=stacked_bands, training=True)
+    return stacked_bands, first_band_open, metadata
+
+
+def calc_rows_cols(metadata: Dict[str, Any]) -> Tuple[int, int]:
+    if not isinstance(metadata, dict):
+        raise ValueError(f"Could not calculate rows and cols for {metadata}")
+    if metadata is None:
+        raise ValueError(f"Could not calculate rows and cols for {metadata}")
+    if not isinstance(metadata["height"], int):
+        raise ValueError(f"Could not calculate rows and cols for {metadata}")
+    if not isinstance(metadata["width"], int):
+        raise ValueError(f"Could not calculate rows and cols for {metadata}")
+    if metadata["height"] < KERNEL_SIZE:
+        raise ValueError(
+            f"Kernel size {KERNEL_SIZE} is bigger than image height {metadata['height']}"
         )
-        # 1. get identifier from tile_name and dates
-        identifier = get_identifier_handler(tile_id=tile_name, dates=dates)
-
-        if identifier is None:
-            continue
-        logger.info(f"Starting download from sentinel aws for {identifier.to_string()}")
-
-        # If Production else use downloader to read from disk
-        band_file_info = download_from_sentinel_aws_handler(
-            identifier=identifier, target_folder=target_folder
+    if metadata["width"] < KERNEL_SIZE:
+        raise ValueError(
+            f"Kernel size {KERNEL_SIZE} is bigger than image width {metadata['width']}"
         )
 
-        if band_file_info is None:
-            continue
+    num_rows = (metadata["height"] - KERNEL_SIZE) // STEP_SIZE + 1
+    num_cols = (metadata["width"] - KERNEL_SIZE) // STEP_SIZE + 1
+    return num_rows, num_cols
 
-        logger.info(f"Starting preprocessing for {identifier.to_string()}")
-        # 3. open dataset readers
-        stacked_bands, first_band_open, original_metadata = open_data_handler(
-            band_file_info=band_file_info
-        )
 
-        if stacked_bands is None:
-            logger.warning(f"Could not open data for {identifier.to_string()}")
-            continue
+def get_small_image(stacked_bands: np.array, window: Window) -> np.ndarray | None:
+    small_image = moving_window(stacked_bands, window)
+    if small_image is None or small_image.max() == 0:
+        return None
+    if small_image.shape != (KERNEL_SIZE, KERNEL_SIZE, 4):
+        small_image = pad_image(small_image, KERNEL_SIZE)
+    return small_image
 
-        metadata = copy.deepcopy(original_metadata)
 
-        num_rows = (metadata["height"] - KERNEL_SIZE) // STEP_SIZE + 1
-        num_cols = (metadata["width"] - KERNEL_SIZE) // STEP_SIZE + 1
+def get_prediction_and_filename(
+    small_image: np.array, identifier: Identifier, file_identifier: int
+) -> Tuple[np.ndarray | None, str | None]:
+    prediction = prediction_handler(small_image=small_image)
+    if prediction is None:
+        return None, None
 
-        stacked_bands = preprocess_handler(array=stacked_bands, training=True)
+    filename = f"{identifier.tile}_{file_identifier}_{identifier.tile_date}.tif"
+    return prediction, filename
 
-        file_identifier = 0
-        for row in range(num_rows + 1):
-            for col in range(num_cols + 1):
-                # Define the window coordinates for the snippet
-                window = rasterio.windows.Window(
-                    col * STEP_SIZE, row * STEP_SIZE, KERNEL_SIZE, KERNEL_SIZE
-                )
 
-                # small_image = preprocess_handler(array=stacked_bands, window=window)
-                small_image = moving_window(stacked_bands, window)
+def process_window(
+    stacked_bands: np.array,
+    window: Window,
+    metadata: Dict[str, Any],
+    first_band_open: dict,
+    identifier: Identifier,
+    file_identifier: int,
+) -> int:
+    small_image = get_small_image(stacked_bands, window)
+    if small_image is None:
+        return file_identifier
 
-                if small_image is None or small_image.max() == 0:
-                    # image is empty
-                    continue
+    prediction, filename = get_prediction_and_filename(
+        small_image, identifier, file_identifier
+    )
+    if prediction is None:
+        return file_identifier
 
-                if small_image.shape != (KERNEL_SIZE, KERNEL_SIZE, 4):
-                    small_image = pad_image(small_image, KERNEL_SIZE)
+    metadata_small_image = update_metadata(
+        metadata=metadata, window=window, first_band_open=first_band_open
+    )
 
-                # 5. send to ml-serve
-                prediction = prediction_handler(small_image=small_image)
-                if prediction is None:
-                    continue
+    write_to_db_handler(
+        prediction=prediction,
+        metadata=metadata_small_image,
+        tile_date=identifier.tile_date,
+        filename=filename,
+    )
+    print("writen to db")
 
-                file_identifier += 1
-                filename = (
-                    f"{identifier.tile}_{file_identifier}_{identifier.tile_date}.tif"
-                )
+    return file_identifier
 
-                # 6. update metadata
-                metadata_small_image = update_metadata(
-                    metadata=metadata, window=window, first_band_open=first_band_open
-                )
 
-                # metadata.update(
-                #     {
-                #         "width": window.width,
-                #         "height": window.height,
-                #         "transform": first_band_open.window_transform(window),
-                #     }
-                # )
-
-                # 7. send to api
-                write_to_db_handler(
-                    prediction=prediction,
-                    metadata=metadata_small_image,
-                    tile_date=identifier.tile_date,
-                    filename=filename,
-                )
-                print("writen to db")
-                # 8. send to aws
-                # ! IMPLEMENTATION NEEDED
-
-        # 4. if MAKE_TRAININGS_DATA: save to disk
-
-        # ! Here the identifier from the API should be used
-        # ! change to raw_data
-        # ToDo: import from constants
-        # folder_list = aws_list_folders(prefix=str(IMAGE_INPUT_DIR))
-        # IMAGE_INPUT_DIR = Path()
-
-    #     saved_total = 0
-
-    #     # for i, tile_folder_path in enumerate(folder_list):
-    #     #     logger.info(f"Processing file {i+1} of {len(folder_list)}")
-    #     try:
-    #         # saved_patches = preprocess_and_save_data(
-    #         #     tile_folder_path=tile_folder_path, masks_gdf=masks_gdf
-    #         # )
-    #         saved_patches = preprocess_and_save_data(
-    #             tile_folder_path=identifier, masks_gdf=masks_gdf
-    #         )
-    #     except Exception as e:
-    #         logger.error(e)
-    #         continue
-
-    #     saved_total += saved_patches
-    #     logger.info(f"Number of files saved: {saved_patches}")
-    # logger.info(f"program finished. Total images saved: {saved_total}")
+if __name__ == "__main__":
+    main()
