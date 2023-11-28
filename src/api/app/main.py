25507a09e7dceddf19a327fc3159985704615ac2
FabKoch <55655975+Taraman12@users.noreply.github.com>
2023-11-28 19:21:36 +0100

    added exception handler

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -1,12 +1,20 @@
 # third party
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from fastapi.responses import PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
 
 # local modules
 from app.api_core.api_v1.api import api_router
 from app.core.config import settings
 from app.db.init_db import init_db
 from app.db.session import SessionLocal
+from app.api_core.errors.http_error import http_error_handler
+from app.api_core.errors.validation_error import http422_error_handler
 
 # TODO: Update description and tags_metadata
 
@@ -56,29 +64,37 @@ tags_metadata = [
     },
 ]
 
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",  # {settings.API_V1_STR}
-    description=description,
-    docs_url=f"{settings.API_V1_STR}/docs",
-    redoc_url=None,
-    openapi_tags=tags_metadata,
-    # root_path="/api/v1",
-)
 
-# Set all CORS enabled origins
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],  # Content-Type
-        expose_headers=["*"],
+def get_application() -> FastAPI:
+    application = FastAPI(
+        title=settings.PROJECT_NAME,
+        openapi_url=f"{settings.API_V1_STR}/openapi.json",  # {settings.API_V1_STR}
+        description=description,
+        docs_url=f"{settings.API_V1_STR}/docs",
+        redoc_url=None,
+        openapi_tags=tags_metadata,
+        # root_path="/api/v1",
     )
 
-app.include_router(api_router, prefix=settings.API_V1_STR)
+    # Set all CORS enabled origins
+    if settings.BACKEND_CORS_ORIGINS:
+        application.add_middleware(
+            CORSMiddleware,
+            allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],  # Content-Type
+            expose_headers=["*"],
+        )
+    application.add_exception_handler(HTTPException, http_error_handler)
+    application.add_exception_handler(RequestValidationError, http422_error_handler)
 
+    application.include_router(api_router, prefix=settings.API_V1_STR)
+
+    return application
+
+
+app = get_application()
 # will be moved to a startup script
 db = SessionLocal()
 init_db(db)
