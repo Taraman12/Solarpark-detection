9789c601e774c86d8bc5378aea26fc0db67255d8
FabKoch <55655975+Taraman12@users.noreply.github.com>
2024-01-09 18:07:52 +0100

    work in progress

--- src/api-preprocessing/app/main.py
+++ src/api-preprocessing/app/main.py
@@ -4,19 +4,21 @@ from fastapi import FastAPI
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import docker
+import logging
 
 app = FastAPI()
-URL_ML = "http://ml-serve:8080"
-URL_MASTER = "http://preprocessing-ma:8000"
-URL_WORKER = "http://preprocessing-w:8000"
+URL_ML = "http://ml-serve"
+URL_MASTER = "http://preprocessing-ma"
+URL_WORKER = "http://preprocessing-w"
+URL_API = "http://main_api/api/v1"
 # client = docker.from_env()
 
 
 class Settings(BaseSettings):
-    USERNAME: str = Field("postgres")
-    PASSWORD: str = Field("postgres")
-    JOIN_TOKEN: str = Field("SWMTKN-1-token")
-    MANAGER_IP: str = Field("10.0.1.10")
+    # USERNAME: str = Field("postgres")
+    # PASSWORD: str = Field("postgres")
+    DOCKER_SWARM_JOIN_TOKEN_MANAGER: str = Field("SWMTKN-1-token")
+    DOCKER_SWARM_MANAGER_IP: str = Field("10.0.1.10")
 
 
 settings = Settings()
@@ -25,6 +27,11 @@ settings = Settings()
 @app.get("/")
 def read_root():
     return {"Hello": f"api-preprocessing is running. URL_ML: {URL_ML}"}
+
+
+@router.get("/health")
+def health_check():
+    return {"status": "Healthy"}
 
 
 @app.get("/env")
@@ -37,9 +44,21 @@ def read_env():
 @app.get("/test_connection-ML")
 def test_connection():
     try:
-        response = requests.get(f"{URL_ML}/ping")
+        response = requests.get(f"{URL_ML}/prediction")
     except requests.exceptions.ConnectionError as e:
         return {"error": f"connection to {URL_ML}/prediction failed with error {e} "}
+    if response.status_code == 200:
+        return {"success": "connection established"}
+    else:
+        return {"error": "connection failed"}
+
+
+@app.get("/test_connection-API")
+def test_connection():
+    try:
+        response = requests.get(f"{URL_API}/")
+    except requests.exceptions.ConnectionError as e:
+        return {"error": f"connection to {URL_API}/ failed with error {e} "}
     if response.status_code == 200:
         return {"success": "connection established"}
     else:
@@ -77,6 +96,7 @@ def docker_token():
         response = client.swarm.init()
         return {"response": response}
     except Exception as e:
+        logging.error(e)
         return {"error": e}
 
 
@@ -85,11 +105,23 @@ def docker_token():
     try:
         client = docker.from_env()
         response = client.swarm.join(
-            remote_addrs=[settings.MANAGER_IP],
-            join_token=settings.JOIN_TOKEN,
+            remote_addrs=[settings.DOCKER_SWARM_MANAGER_IP],
+            join_token=settings.DOCKER_SWARM_JOIN_TOKEN_MANAGER,
         )
         return {"response": response}
     except Exception as e:
+        logging.error(e)
+        return {"error": e}
+
+
+@app.delete("/leave-swarm")
+def leave_swarm():
+    try:
+        client = docker.from_env()
+        response = client.swarm.leave()
+        return {"response": response}
+    except Exception as e:
+        logging.error(e)
         return {"error": e}
 
 
