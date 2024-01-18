06300893fe38845cb2e1393ba2fd340341e43310
FabKoch <55655975+Taraman12@users.noreply.github.com>
2024-01-18 18:21:35 +0100

    changed navbar to not rounded and header

--- src/new-frontend/src/components/AsideMenuLayer.vue
+++ src/new-frontend/src/components/AsideMenuLayer.vue
@@ -33,12 +33,12 @@ const asideLgCloseClick = (event) => {
 <template>
   <aside
     id="aside"
-    class="lg:py-2 lg:pl-2 w-60 fixed flex z-40 top-0 h-screen transition-position overflow-hidden"
+    class="w-60 fixed flex z-40 top-0 h-screen transition-position overflow-hidden"
   >
-    <div class="aside lg:rounded-2xl flex-1 flex flex-col overflow-hidden dark:bg-slate-900">
+    <div class="aside flex-1 flex flex-col overflow-hidden dark:bg-slate-900">
       <div class="aside-brand flex flex-row h-14 items-center justify-between dark:bg-slate-900">
         <div class="text-center flex-1 lg:text-left lg:pl-6 xl:text-center xl:pl-0">
-          <b class="font-black">One</b>
+          <b class="font-black">Solar Park Detection</b>
         </div>
         <button class="hidden lg:inline-block xl:hidden p-3" @click.prevent="asideLgCloseClick">
           <BaseIcon :path="mdiClose" />
